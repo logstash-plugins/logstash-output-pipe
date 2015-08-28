0e9ca30df8338f764432167287329a2f69bd623c
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-08-28 22:55:36 +0200

    add basic send test to the pipe
    Fixes #2

--- spec/outputs/pipe_spec.rb
+++ spec/outputs/pipe_spec.rb
@@ -3,7 +3,7 @@ require_relative "../spec_helper"
 require "logstash/plugin"
 require "logstash/event"
 
-describe "LogStash::Outputs::Pipe" do
+describe LogStash::Outputs::Pipe do
 
   let(:format) { "%{message}" }
   let(:cmd)    { "cmd" }
@@ -13,4 +13,23 @@ describe "LogStash::Outputs::Pipe" do
     expect { plugin.register }.to_not raise_error
   end
 
+  describe "#send" do
+
+    subject { LogStash::Outputs::Pipe.new("command" => cmd) }
+
+    let(:properties) { { "message" => "This is a message!"} }
+    let(:event)      { LogStash::Event.new(properties) }
+
+    let(:pipe)       { double("pipe") }
+    before(:each) do
+      allow(PipeWrapper).to receive(:new).and_return(pipe)
+      subject.register
+    end
+
+    it "sends the generated event to the pipe" do
+      expect(pipe).to receive(:puts).with(event.to_json)
+      subject.receive(event)
+    end
+
+  end
 end
