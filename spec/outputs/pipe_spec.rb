27ca0d79d5e71050629d9fd26a0e27b5bf002e99
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-08-28 22:42:59 +0200

    add basic registration test for the plugin
    Fixes #2

--- spec/outputs/pipe_spec.rb
+++ spec/outputs/pipe_spec.rb
@@ -1,1 +1,16 @@
-require "logstash/devutils/rspec/spec_helper"
+# encoding: utf-8
+require_relative "../spec_helper"
+require "logstash/plugin"
+require "logstash/event"
+
+describe "LogStash::Outputs::Pipe" do
+
+  let(:format) { "%{message}" }
+  let(:cmd)    { "cmd" }
+
+  it "should register without errors" do
+    plugin = LogStash::Plugin.lookup("output", "pipe").new({"command" => cmd})
+    expect { plugin.register }.to_not raise_error
+  end
+
+end
